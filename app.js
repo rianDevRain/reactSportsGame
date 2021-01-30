cb1c846161d71c02e59d82e49b36611abf16491d
Rian McCollom <mail@rian.dev>
2021-01-29 23:42:03 -0600

    Fixed shotHandler-Fixed CSS

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
+/** @format */
+
 function Team(props) {
 	let shotPercentageDiv;
-	
+
 	if (props.stats.shots) {
 		const shotPercentage = Math.round((props.stats.score / props.stats.shots) * 100);
 		shotPercentageDiv = (
 			<div>
 				<strong>Shooting % {shotPercentage}</strong>
 			</div>
-		)
+		);
 	}
-	
+
 	return (
 		<div className="Team">
 			<h2>{props.name}</h2>
@@ -28,9 +30,11 @@ function Team(props) {
 
 			{shotPercentageDiv}
 
-			<button onClick={props.shotHanler}>Shoot:</button>
+			<button className="buttons" onClick={props.shotHandler}>
+				Shoot:
+			</button>
 		</div>
-	)
+	);
 }
 
 function ScoreBoard(props) {
@@ -39,85 +43,78 @@ function ScoreBoard(props) {
 			<div className="teamStats">
 				<h3>VISITORS</h3>
 				<h3>{props.visitingTeamStats.score}</h3>
-			</div>p
-
+			</div>
 			<h3>SCOREBOARD</h3>
-
 			<div className="teamStats">
 				<h3>HOME</h3>
 				<h3>{props.homeTeamStats.score}</h3>
 			</div>
 		</div>
-	)
+	);
 }
 
 class Game extends React.Component {
 	constructor(props) {
-		super(props)
+		super(props);
 
 		this.state = {
 			resetCount: 0,
 			homeTeamStats: {
 				shots: 0,
-				score: 0
+				score: 0,
 			},
 			visitingTeamStats: {
 				shots: 0,
-				score: 0
-			}
-		}
+				score: 0,
+			},
+		};
 
 		this.shotSound = new Audio("./media/audio/metalbat.wav");
 		this.scoreSound = new Audio("./media/audio/yell4yeeha.wav");
-	
+
 		// this.shoot = this.shoot.bind(this)
 	}
 
-	
-
 	shoot = (team) => {
-		const teamStatsKey = `${team}TeamStats`
-		let score = this.state[teamStatsKey].score
-		this.shotSound.play()
+		console.log(team);
+		const teamStatsKey = `${team}TeamStats`;
+		let score = this.state[teamStatsKey].score;
+		this.shotSound.play();
 
 		if (Math.random() > 0.5) {
-			score += 1
+			score += 1;
 
 			setTimeout(() => {
-				this.scoreSound.play()
-			}, 400)
+				this.scoreSound.play();
+			}, 100);
 		}
 
 		this.setState((state, props) => ({
 			[teamStatsKey]: {
 				shots: state[teamStatsKey].shots + 1,
 				score,
-			}
-		}))
-	}
+			},
+		}));
+	};
 
 	resetGame = () => {
 		this.setState((state, props) => ({
 			resetCount: state.resetCount + 1,
 			homeTeamStats: {
 				shots: 0,
-				score: 0
+				score: 0,
 			},
 			visitingTeamStats: {
 				shots: 0,
-				score: 0
-			}
-		}))
-	}
+				score: 0,
+			},
+		}));
+	};
 
 	render() {
 		return (
 			<div className="Game">
-				<ScoreBoard 
-					visitingTeamStats={this.state.visitingTeamStats} 
-					homeTeamStats={this.state.homeTeamStats} 
-
-					/>
+				<ScoreBoard visitingTeamStats={this.state.visitingTeamStats} homeTeamStats={this.state.homeTeamStats} />
 
 				<h1>Welcome to {this.props.venue}</h1>
 				<div className="stats">
@@ -130,21 +127,21 @@ class Game extends React.Component {
 
 					<div className="versus">
 						<h1>VS</h1>
-						<div>
-							<strong>Resets:</strong> {this.state.resetCount}
-							<button onClick={this.resetGame}>Reset Game</button>
-						</div>
+						<strong>Resets:</strong> {this.state.resetCount}
+						<button className="buttons" onClick={this.resetGame}>
+							Reset Game
+						</button>
 					</div>
 
 					<Team
 						name={this.props.homeTeam.name}
 						logo={this.props.homeTeam.logoSrc}
 						stats={this.state.homeTeamStats}
-						shotHanler={() => this.shoot("home")}
+						shotHandler={() => this.shoot("home")}
 					/>
 				</div>
 			</div>
-		)
+		);
 	}
 }
 
@@ -152,43 +149,29 @@ function App(props) {
 	const voodoo = {
 		name: "Voodoo Boys",
 		logoSrc: "./media/images/pic1.jpg",
-	}
+	};
 
 	const mox = {
 		name: "The Mox",
 		logoSrc: "./media/images/pic2.jpg",
-	}
+	};
 
 	const alde = {
 		name: "Aldecaldos",
 		logoSrc: "./media/images/pic3.jpg",
-	}
-	
+	};
+
 	const tyger = {
 		name: "Tyger Claws",
 		logoSrc: "./media/images/pic4.jpg",
-	}
+	};
 
 	return (
 		<div className="App">
-			<Game 
-				venue="Night City" 
-				homeTeam={voodoo} 
-				visitingTeam={mox} 
-
-				/>
-			<Game 
-				venue="The Badlands" 
-				homeTeam={alde} 
-				visitingTeam={tyger} 
-
-				/>
+			<Game venue="Night City" homeTeam={voodoo} visitingTeam={mox} />
+			<Game venue="The Badlands" homeTeam={alde} visitingTeam={tyger} />
 		</div>
-	)
+	);
 }
 
-
-ReactDOM.render(
-	<App />, 
-	document.getElementById("root")
-)
+ReactDOM.render(<App />, document.getElementById("root"));
